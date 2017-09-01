a73425cfaa648878280f8980d262ba0f3b279c42
Sven Kroemeke <skroemeke@luxoft.com>
2017-09-01 17:42:45 +0200

    Class like

--- src/main.rs
+++ src/main.rs
@@ -8,52 +8,78 @@ use std::io;
 use std::env;     // args
 use std::fs::{self, DirEntry};  // directory
 use std::path::Path;  // path, clear
+use std::iter::*;
 
 use id3::Tag;
 
-/// The function that runs from the starting point
-fn visit_dirs(dir: &Path, cb: &Fn(&DirEntry) -> io::Result<()> ) -> io::Result<()> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                visit_dirs(&path, cb)?;
-            } else {
-                cb(&entry)?;
-            }
+/// generell info
+struct InfoAlbum <'b> {
+    album_name : &'b str,
+    reference_path : Vec<&'b Path>,
+}
+
+struct Collection<'b> { collection : Vec<&'b InfoAlbum<'b> > }
+
+impl <'b> Collection <'b> {
+
+    pub fn new() -> Collection<'b>  {
+        Collection { 
+            collection : Vec::new() 
         }
     }
-    Ok(())
-}
 
-/// the function to check all files separately
-fn visit_files(cb: &DirEntry) -> io::Result<()> {
-    let filetype = tree_magic::from_filepath(&cb.path());
-    match filetype.as_ref() {
-        "text/plain" => {},
-        "audio/mpeg" => visit_audio_files(&cb.path()),
-        _ => println!("[{:?}]{:?}",filetype, cb.path()),
+    fn is_already_in_collection(&self, piece: &str) -> bool {
+        //self.collection.iter().position(|&&album_name| album_name == piece) //find(album_name == piece)
+        true
     }
-	Ok(())	
-}
 
-fn visit_audio_files(cb: &Path) {
-    let tag = Tag::read_from_path(cb).unwrap();
-    println!("{:?}",tag.artist().unwrap())
-}
+    /// The function that runs from the starting point
+    fn visit_dirs(&self, dir: &Path, cb: &Fn(&DirEntry) -> io::Result<()> ) -> io::Result<()> {
+        if dir.is_dir() {
+            for entry in fs::read_dir(dir)? {
+                let entry = entry?;
+                let path = entry.path();
+                if path.is_dir() {
+                    self.visit_dirs(&path, cb)?;
+                } else {
+                    cb(&entry)?;
+                }
+            }
+        }
+        Ok(())
+    }
 
+    /// the function to check all files separately
+    fn visit_files(cb: &DirEntry) -> io::Result<()> {
+        let filetype = tree_magic::from_filepath(&cb.path());
+        match filetype.as_ref() {
+            "text/plain" => {},
+            "audio/mpeg" => Collection::visit_audio_files(&cb.path()),
+            _ => println!("[{:?}]{:?}",filetype, cb.path()),
+        }
+    	Ok(())	
+    }
+
+    fn visit_audio_files(cb: &Path) {
+        let tag : Tag = Tag::read_from_path(cb).unwrap();
+
+        println!("{:?}",tag.artist().unwrap())
+    }
+}
 
 
 fn main() {
     let args: Vec<_> = env::args().collect();
 
-    let mut path = ".";
+    let collection : Collection<'static> = Collection::new();
+
+
+    let mut path : &str = ".";
     if args.len() > 1 {
         path = &args[1];
     }
     let real_path = Path::new(path);
-    match visit_dirs(real_path, &visit_files) {
+    match collection.visit_dirs(real_path, &Collection::visit_files) {
         _ => println!("Finished!")
     }
 }
