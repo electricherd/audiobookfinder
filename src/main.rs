c91660e6b92eac4f8105fccf7c93323cc50101c8
ElectricHerd <skroemeke@gmail.com>
2017-09-04 19:51:24 +0200

    Trying container

--- src/main.rs
+++ src/main.rs
@@ -8,41 +8,45 @@ use std::io;
 use std::env;     // args
 use std::fs::{self, DirEntry};  // directory
 use std::path::Path;  // path, clear
-use std::iter::*;
+use std::collections::HashMap;
+
 
 use id3::Tag;
 
 /// generell info
 struct InfoAlbum <'b> {
-    album_name : &'b str,
     reference_path : Vec<&'b Path>,
 }
 
-struct Collection<'b> { collection : Vec<&'b InfoAlbum<'b> > }
+struct Collection<'b> {     
+    collection : HashMap<&'b str, &'b InfoAlbum<'b> > 
+}
 
-impl <'b> Collection <'b> {
+type FileFn<'a,'b: 'a> = &'a Fn(&'a mut Collection, &'b DirEntry) -> io::Result<()>;
 
-    pub fn new() -> Collection<'b>  {
+impl <'a,'b: 'a> Collection <'a> {
+
+    pub fn new() -> Collection<'a>  {
         Collection { 
-            collection : Vec::new() 
+            collection : HashMap::new() 
         }
     }
 
     fn is_already_in_collection(&self, piece: &str) -> bool {
-        //self.collection.iter().position(|&&album_name| album_name == piece) //find(album_name == piece)
-        true
+        self.collection.contains_key(piece)
     }
 
     /// The function that runs from the starting point
-    fn visit_dirs(&self, dir: &Path, cb: &Fn(&DirEntry) -> io::Result<()> ) -> io::Result<()> {
+    fn visit_dirs(&mut self, dir: &'b Path, cb: FileFn<'a,'b>) -> io::Result<()> {
         if dir.is_dir() {
+            let &mut test : &mut Collection<'a> = self;
             for entry in fs::read_dir(dir)? {
                 let entry = entry?;
                 let path = entry.path();
                 if path.is_dir() {
                     self.visit_dirs(&path, cb)?;
                 } else {
-                    cb(&entry)?;
+                    cb(test, &entry)?;
                 }
             }
         }
@@ -50,19 +54,18 @@ impl <'b> Collection <'b> {
     }
 
     /// the function to check all files separately
-    fn visit_files(cb: &DirEntry) -> io::Result<()> {
+    fn visit_files(col: &mut Collection, cb: &'b DirEntry) -> io::Result<()> {
         let filetype = tree_magic::from_filepath(&cb.path());
         match filetype.as_ref() {
             "text/plain" => {},
-            "audio/mpeg" => Collection::visit_audio_files(&cb.path()),
+            "audio/mpeg" => col.visit_audio_files(&cb.path()),
             _ => println!("[{:?}]{:?}",filetype, cb.path()),
         }
     	Ok(())	
     }
 
-    fn visit_audio_files(cb: &Path) {
+    fn visit_audio_files(&mut self, cb: &'b Path) {
         let tag : Tag = Tag::read_from_path(cb).unwrap();
-
         println!("{:?}",tag.artist().unwrap())
     }
 }
@@ -71,8 +74,7 @@ impl <'b> Collection <'b> {
 fn main() {
     let args: Vec<_> = env::args().collect();
 
-    let collection : Collection<'static> = Collection::new();
-
+    let mut collection : Collection<'static> = Collection::new();
 
     let mut path : &str = ".";
     if args.len() > 1 {
