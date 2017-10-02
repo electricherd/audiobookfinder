862b4a996fcc915312a53335b37c92c3701fdf3d
ElectricHerd <skroemeke@gmail.com>
2017-10-02 11:50:42 +0200

    A bit nicer, new ideas.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,79 @@
+//! A simple first program in rust, actually I plan to do something even
+//! useful. Trying to find all my audiobooks on many machines, identify them,
+//! find duplicates (later trying to solve the problem, also including their permissions, different names, but same albums, etc),
+//! get all stats about it).
+
 //#![feature(alloc_system)]
 //extern crate alloc_system;   // strip down size of binary executable
 
+//extern crate hyper;   // sometime, for a good server / client over https communication
 extern crate id3;
 extern crate tree_magic;  // mime types
 extern crate rayon;
+extern crate hostname;
 
-use id3::Tag;
+use id3::Tag;     // to identify the audio files
 
-use std::io;
+use std::io;      // reading files 
 use std::env;     // args
 use std::cmp;     // max
-use std::fs::{self, DirEntry};  // directory
+use std::fs::{self, DirEntry, Permissions};  // directory
 use std::path::{Path,PathBuf};  // path, clear
-use std::collections::hash_map::{HashMap,Entry};
-use std::sync::{Arc, Mutex};
+use std::collections::hash_map::{HashMap,Entry}; // my main item uses a hash map
+use std::sync::{Arc, Mutex};                     // safe containment and locking
+//use std::os::windows::fs::MetadataExt;
+use std::os::linux::fs::MetadataExt;
 
-use rayon::prelude::*;
 
-/// general info
-struct InfoAlbum {
-    reference_path : Vec<PathBuf>
+use rayon::prelude::*;                           // threading with iterators
+
+
+
+#[allow(dead_code)]
+/// # File info
+/// All info on a audio files
+/// (let's see how info we need,
+/// its size vs necessary info)
+struct FileInfo {
+    path : PathBuf,
+    size : u64,
+    permissions: Permissions
 }
 
-struct Collection {     
+
+/// # Album information
+/// General info
+struct InfoAlbum {
+    reference_path : Vec<FileInfo>
+}
+
+#[allow(dead_code)]
+struct Worker {
+    /// identify them.
+    id       : u32, 
+    hostname : String,
+    max_threads : usize,
+}
+
+/// # Worker
+/// the worker is supposed to be running on different machines
+/// with one server and many clients
+impl Worker {
+    /// # Just new
+    /// To identify how to comment
+    /// # Arguments
+    /// * '_hostname' - The hostname from remote/local
+    /// * '_id' - the identification (each will create an own hash)
+    /// * '_maxthreads' - how many threads can the worker create
+    pub fn new(_hostname: String, _id: u32, _maxthreads : usize) -> Worker {
+        Worker { hostname: _hostname, id : _id, max_threads: _maxthreads}
+    }
+}
+
+
+struct Collection {    
+    /// This collection contains all data
+    who         : Worker,
     collection  : HashMap<String, Box<InfoAlbum>>,
     stats       : Stats
 }
@@ -30,6 +81,7 @@ struct Collection {
 struct Stats {
     files : Files,
     audio : Audio,
+    threads : u8,
 }
 
 struct Audio {
@@ -47,14 +99,16 @@ struct Files {
 
 type FileFn = Fn(&mut Collection, &DirEntry) -> io::Result<()>;
 
+/// This part implements all functions
 impl Collection {
-
-    pub fn new() -> Collection {
+    pub fn new(_hostname: String, _numthreads: usize) -> Collection {
         Collection { 
+            who   :  Worker::new(_hostname, 0, _numthreads),
             collection : HashMap::new(), 
             stats      : Stats { 
                           files: Files {analyzed: 0, faulty: 0, searched: 0, other: 0},
                           audio: Audio {albums: 0, max_songs: 0},
+                          threads: 0
                         }
         }
     }
@@ -83,7 +137,7 @@ impl Collection {
         let filetype = tree_magic::from_filepath(&cb.path());
         let prefix = filetype.split("/").nth(0);
         match prefix {
-            Some("audio") => col.visit_audio_files(&cb.path()),
+            Some("audio") => col.visit_audio_files(&cb.path())?,
             Some("text") | Some("application") 
              | Some("image") => {},
             _ => {
@@ -94,22 +148,30 @@ impl Collection {
     	Ok(())	
     }
 
-    fn visit_audio_files(&mut self, cb: &Path) {
+    fn visit_audio_files(&mut self, cb: &Path) -> io::Result<()> {
+
         match Tag::read_from_path(cb) {
             Ok(tag) => {
                 if let Some(artist) = tag.artist() {
                     self.stats.files.analyzed += 1;                    
 
                     let path_buffer = cb.to_path_buf();
+
+                    let metadata = fs::metadata(cb)?;
+                    let filesize = metadata.st_size();
+                    let permissions = metadata.permissions();
+
+                    let possible_entry = FileInfo{path: path_buffer, size : filesize, permissions: permissions};
+
                     match self.collection.entry(String::from(artist)) {
                         Entry::Occupied(mut entry) => {
-                            entry.get_mut().reference_path.push(path_buffer);
+                            entry.get_mut().reference_path.push(possible_entry);
 
                             let this_albums_length = entry.get().reference_path.len();
-                            self.stats.audio.max_songs = cmp::max(self.stats.audio.max_songs,this_albums_length)
+                            self.stats.audio.max_songs = cmp::max(self.stats.audio.max_songs,this_albums_length);
                         }
                         Entry::Vacant(entry) => {
-                            let this_album : InfoAlbum = InfoAlbum { reference_path: vec!(path_buffer) };
+                            let this_album : InfoAlbum = InfoAlbum { reference_path: vec!(possible_entry) };
                             entry.insert(Box::new(this_album));
                             self.stats.audio.albums += 1;
                         }
@@ -120,9 +182,11 @@ impl Collection {
             },
             Err(_) => { self.stats.files.faulty += 1}
         }
+        Ok(())
    }
 
    pub fn print_stats(&self) {
+        println!("pathes/threads       : {:?}", self.stats.threads);     
         println!("albums found         : {:?}", self.stats.audio.albums); 
         println!("most songs per album : {:?}", self.stats.audio.max_songs); 
         println!("----------------------");
@@ -151,21 +215,25 @@ fn parse_args(args: Vec<String>) -> Vec<String> {
 fn main() {
     let args : Vec<_> = env::args().collect();
 
-    // take ownership
+    // parse
     let all_pathes = parse_args(args);
+
+    let hostname = hostname::get_hostname().unwrap_or("undefined".to_string());
     
-    // initialize collection
-    let collection_protected = Arc::new(Mutex::new(Collection::new()));
+    //
+    let init_collection = Collection::new(hostname, rayon::current_num_threads());
+    let collection_protected = Arc::new(Mutex::new(init_collection));
 
     all_pathes.par_iter().for_each(|elem| {
+        //println!("Start thread number# {:?}", rayon::current_thread_index());
         let live_here = collection_protected.clone();
-        let _ = live_here.lock().unwrap().visit_dirs(Path::new(elem),&Collection::visit_files);
 
+        let mut pure_collection = live_here.lock().unwrap();
+        let _ = pure_collection.visit_dirs(Path::new(elem),&Collection::visit_files);
     });
 
-    let collection = collection_protected.lock().unwrap();
-    collection.print_stats();
+    let result_collection = collection_protected.lock().unwrap();
+    result_collection.print_stats();
     
-
     println!("Finished!");
 }
