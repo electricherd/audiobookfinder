c549279051dca51ac610110e43bbfb411a7c61c2
ElectricHerd <skroemeke@gmail.com>
2018-03-04 22:36:24 +0100

    Trying cross compiling #5 trying newer arm version, own document script.

---
+++ ci/document.sh
@@ -0,0 +1,8 @@
+#!bash
+# only document on i686
+if [ $TARGET = i686-unknown-linux-gnu ]; then
+   cargo doc --no-deps \
+   && echo '<meta http-equiv=refresh content=0;url=${CRATE_NAME}/index.html>' > target/doc/index.html \
+   &&  ghp-import -n target/doc \
+   &&  git push -qf https://${gitHubToken}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages;
+fi
