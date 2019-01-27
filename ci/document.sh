a40ff2284d6ee947a10151b389c994b50e49b337
ElectricHerd <skroemeke@gmail.com>
2019-01-27 15:42:53 +0100

    Don't build run, rather run, and deploy doc as a correct link.

--- ci/document.sh
+++ ci/document.sh
@@ -1,8 +1,9 @@
 #!bash
 # only document on i686 and xenial and stable
 if [ $TARGET = x86_64-unknown-linux-gnu ] && [ $UBUNTU_VER = LTS_16.04 ] && [ $TRAVIS_RUST_VERSION = stable ]; then
+   echo "Deploying documentation of '${CRATE_NAME}'!!"
    cargo doc --no-deps \
-   && echo '<meta http-equiv=refresh content=0;url=${CRATE_NAME}/index.html>' > target/doc/index.html \
+   && echo "<meta http-equiv=refresh content=0;url=${CRATE_NAME}/index.html>" > target/doc/index.html \
    && ghp-import -n target/doc \
    && git push -qf https://${gitHubToken}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
 fi
