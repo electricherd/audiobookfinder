fa983583e4b74a19b41016f719a578226e1e327d
electricherd <electricherd@users.noreply.github.com>
2020-07-06 14:10:59 +0200

    [net] put mdns into libp2p swarm, using kademlia as organisatorial behaviour patter, fix ui

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -3,34 +3,21 @@
 //! It also let's us startup and perform everything in yet one step.
 mod data;
 pub mod key_keeper;
-mod noise;
+mod net_actors;
+mod ui_data;
 
-use super::ctrl::{self, ForwardNetMessage};
+use super::ctrl;
+use ui_data::UiData;
 
-use async_std::{
-    sync::{Arc as AArc, Mutex as AMutex},
-    task::{self, Context, Poll},
-};
-use futures::future::poll_fn;
-use futures::{future::Future, prelude::*};
-use futures_util::{pin_mut, StreamExt};
+use async_std::task::{self, Context, Poll};
+use futures::prelude::*;
+use futures_util::StreamExt;
 use libp2p::{
-    identify::{Identify, IdentifyEvent},
-    mdns::{
-        service::{self, MdnsPacket, MdnsPeer},
-        MdnsService,
-    },
-    ping::{self, Ping, PingConfig, PingEvent},
-    swarm::NetworkBehaviourEventProcess,
+    kad::{record::store::MemoryStore, Kademlia},
+    mdns::Mdns,
     PeerId, Swarm,
 };
-use std::{
-    self,
-    error::Error,
-    io,
-    sync::{mpsc::Sender, Arc, Mutex},
-    time::Duration,
-};
+use std::{self, error::Error, sync::mpsc::Sender};
 
 /// The Net component keeps control about everything from net.
 ///
@@ -42,18 +29,13 @@ use std::{
 pub struct Net {
     // todo: don't forget, that there are peer methods and life time for this list entries, it
     //       may be not a good idea to keep it like this, even though it's their ids!
-    clients_connected: Arc<Mutex<Vec<PeerId>>>,
     has_ui: bool,
     ui_sender: Sender<ctrl::UiUpdateMsg>,
 }
 
 impl Net {
     pub fn new(has_ui: bool, ui_sender: Sender<ctrl::UiUpdateMsg>) -> Self {
-        Net {
-            clients_connected: Arc::new(Mutex::new(Vec::new())),
-            has_ui,
-            ui_sender,
-        }
+        Net { has_ui, ui_sender }
     }
 
     /// Lookup yet is the start of the networking.
@@ -64,77 +46,46 @@ impl Net {
         // to controller messages (mostly tui now)
         let ui_update_sender = self.ui_sender.clone();
 
-        // collection of addresses
-        let borrow_arc_connected_clients = self.clients_connected.clone();
-
-        // statistics
-        let mut count_valid = 0;
-
         // prepare everything for mdns thread
         let my_peer_id = key_keeper::get_p2p_server_id();
-        Self::async_mdns_discover(
-            &my_peer_id,
-            &ui_update_sender,
-            has_ui,
-            borrow_arc_connected_clients,
-            &mut count_valid,
-        )
-        .await
-        .and_then(|count_response| {
-            if !has_ui {
-                let output_string = format!(
-                    "no response from : \n\
-                     ----------------- {no_resp:>width$}\n",
-                    no_resp = count_response,
-                    width = 3
-                );
-                info!("{}", output_string);
-            }
-            Ok(())
-        })
-        .unwrap();
+        Self::build_swarm_and_run(&my_peer_id, &ui_update_sender, has_ui)
+            .await
+            .unwrap();
     }
 
     /// Discovers mdns on the net and should have a whole
     /// process with discovered clients to share data.
-    async fn async_mdns_discover(
-        own_peer_id: &PeerId,
+    async fn build_swarm_and_run(
+        _own_peer_id: &PeerId,
         ctrl_sender: &std::sync::mpsc::Sender<ctrl::UiUpdateMsg>,
         has_ui: bool,
-        borrow_arc_connected_clients: Arc<Mutex<Vec<PeerId>>>,
-        count_valid: &mut u32,
-    ) -> Result<u32, Box<dyn Error>> {
+    ) -> Result<(), Box<dyn Error>> {
         let local_key = &*key_keeper::SERVER_KEY;
         let local_peer_id = PeerId::from(local_key.public());
 
         // get the transporter
-        let transport = noise::build_noise_transport(
+        let transport = net_actors::build_noise_transport(
             &*key_keeper::SERVER_KEY,
             Some(*key_keeper::PRESHARED_SECRET),
         );
 
         let mut swarm = {
-            let behaviour = noise::CustomBehaviour {
-                identify: Identify::new(
-                    "adbf/0.1.0".into(),
-                    "adbf-agent".into(),
-                    local_key.public(),
-                ),
-                ping: Ping::new(PingConfig::new()),
+            // Create a Kademlia behaviour.
+            let store = MemoryStore::new(local_peer_id.clone());
+            let kademlia = Kademlia::new(local_peer_id.clone(), store);
+            let ui_data = UiData::new(if has_ui {
+                Some(ctrl_sender.clone())
+            } else {
+                None
+            });
+
+            let behaviour = net_actors::CustomBehaviour {
+                kademlia,
+                mdns: Mdns::new()?,
+                ui_data,
             };
             Swarm::new(transport, behaviour, local_peer_id.clone())
         };
-
-        //
-        Swarm::listen_on(&mut swarm, "/ip4/0.0.0.0/tcp/0".parse()?)?;
-
-        let mut count_no_response: u32 = 0;
-
-        let mut service = MdnsService::new()?;
-
-        //pin_mut!(service);
-
-        info!("Started Mdns Service!");
 
         // start animation
         if has_ui {
@@ -146,141 +97,29 @@ impl Net {
                 ))
                 .unwrap();
         }
+        // kick off the network actor framework
+        Swarm::listen_on(&mut swarm, "/ip4/0.0.0.0/tcp/0".parse().unwrap()).unwrap();
 
-        info!("Starting Mdns Looping!");
-        // todo: to gracefully stop here, inside the loop could be a receive, which in an
-        //       async select! block or just by try_select waits for a terminate message
-        //       through a channel.
-        let poller = futures::future::poll_fn(|cx: &mut Context| {
-            let mut listening = false;
+        let mut listening = false;
+        task::block_on(future::poll_fn(move |cx: &mut Context| {
+            // this is just future polling for the sake of running swarm
+            // and to catch some actions in order for debug messages, NOT MORE
+            // because real actions are supposed to be done using the actors!
             loop {
                 match swarm.poll_next_unpin(cx) {
-                    Poll::Ready(Some(event)) => {
-                        info!("{:?}", event);
-                    }
-                    Poll::Ready(None) => return Poll::Ready(Ok::<(), ()>(())),
-                    Poll::Pending => {
-                        if !listening {
-                            for addr in Swarm::listeners(&mut swarm) {
-                                //info!("Address {} - {}", addr, local_peer_id);
-                                listening = true;
-                            }
-                        }
-                    }
+                    Poll::Ready(Some(_)) => info!("ha"),
+                    Poll::Ready(None) | Poll::Pending => break,
                 }
             }
-            Poll::Pending
-        });
-
-        Self::poll_mdns(service, has_ui, own_peer_id).await;
-        Ok(count_no_response)
-    }
-
-    async fn poll_mdns(mut service: MdnsService, has_ui: bool, own_peer_id: &PeerId) {
-        loop {
-            trace!("taking new package ...");
-            let (mut srv, packet) = service.next().await;
-            Self::take_mdns(&mut srv, packet, has_ui, own_peer_id);
-            service = srv;
-        }
-    }
-
-    fn take_mdns(
-        service: &mut MdnsService,
-        packet: MdnsPacket,
-        has_ui: bool,
-        own_peer_id: &PeerId,
-    ) {
-        match packet {
-            MdnsPacket::Query(query) => {
-                // We detected a libp2p mDNS query on the network. In a real application, you
-                // probably want to answer this query by doing `query.respond(...)`.
-                trace!("Query came in from {:?}", query.remote_addr());
-                if !has_ui {
-                    println!("Query came in from: {:?}", query.remote_addr());
-                }
-
-                // send back own peer ?? todo: a bit more maybe? addresses?
-                let response = service::build_query_response(
-                    query.query_id(),
-                    own_peer_id.clone(),
-                    vec![].into_iter(), // something or leave it empty??
-                    Duration::from_secs(120),
-                )
-                .unwrap();
-                service.enqueue_response(response);
-            }
-            MdnsPacket::Response(response) => {
-                // We detected a libp2p mDNS response on the network. Responses are for
-                // everyone and not just for the requester, which makes it possible to
-                // passively listen.
-                for new_peer in response.discovered_peers() {
-                    // if found myself then don't connect
-                    if own_peer_id != new_peer.id() {
-                        info!("Discovered: {:?}", new_peer.id());
-                        if !has_ui {
-                            println!("Discovered: {:?}", new_peer.id());
-                        }
-                        // These are the self-reported addresses of the peer we just discovered.
-                        for addr in new_peer.addresses() {
-                            trace!(" Address = {:?}", addr);
-                        }
-                    } else {
-                        trace!("Found myself: {:?}", new_peer.id());
-                        // to terminal
-                        if !has_ui {
-                            println!("Found myself: {:?}", new_peer.id());
-                        }
-                    }
+            if !listening {
+                for addr in libp2p::Swarm::listeners(&swarm) {
+                    info!("Listening on {:?}", addr);
+                    listening = true;
                 }
             }
-            MdnsPacket::ServiceDiscovery(query) => {
-                // The last possibility is a service detection query from DNS-SD.
-                // Just like `Query`, in a real application you probably want to call
-                // `query.respond`.
-                info!("Detected service query from {:?}", query.remote_addr());
-            }
-        }
-    }
-
-    async fn connect_new_clients(
-        new_peer: &MdnsPeer,
-        peers_connected: AArc<AMutex<Vec<PeerId>>>,
-        ctrl_sender: &std::sync::mpsc::Sender<ctrl::UiUpdateMsg>,
-        has_ui: bool,
-    ) {
-        if let Some(mut guard) = peers_connected.try_lock() {
-            let all_stored_peers = &mut *guard;
-            // search if ip address is already collected
-            if all_stored_peers
-                .iter()
-                .all(|stored_peer| stored_peer != new_peer.id())
-            {
-                let count = all_stored_peers.len();
-                if has_ui {
-                    ctrl_sender
-                        .send(ctrl::UiUpdateMsg::NetUpdate(ForwardNetMessage::new(
-                            ctrl::NetMessages::ShowNewHost,
-                            new_peer.id().to_string(),
-                        )))
-                        .unwrap();
-                    ctrl_sender
-                        .send(ctrl::UiUpdateMsg::NetUpdate(ForwardNetMessage::new(
-                            ctrl::NetMessages::ShowStats {
-                                show: ctrl::NetStats {
-                                    line: count,
-                                    max: 0, //index,
-                                },
-                            },
-                            String::from(""),
-                        )))
-                        .unwrap();
-                }
-                // put into collection to not find again
-                all_stored_peers.push(new_peer.id().clone());
-            }
-        }
-        ()
+            Poll::<()>::Pending
+        }));
+        Ok(())
     }
 }
 
---
+++ src/net/net_actors.rs
@@ -0,0 +1,136 @@
+/// The net will be represented by a swarm as in libp2p
+/// https://docs.rs/libp2p/latest/libp2p/swarm/index.html.
+///
+/// Because swarm is the manager a state machine as before could be replaced, also the working
+/// Mdns Server/Client can just be transparently added used in the behavior of this network.
+/// As transport layer, an experimental but prospering protocol called "noise protocol" will
+/// be used.
+/// The network communication is basically an actor system just as used here in the webui
+/// with actix actor, which btw could maybe also replaced by the websocket protocol from
+/// libp2p, but for now it will stay in a nice, small http server.
+///
+/// The noise protocol to be used
+/// (http://noiseprotocol.org/)
+///
+use super::ui_data::UiData;
+
+use async_std::io;
+use libp2p::{
+    kad::{
+        record::store::MemoryStore, Kademlia, KademliaEvent, PeerRecord, PutRecordOk, QueryResult,
+        Record,
+    },
+    mdns::{Mdns, MdnsEvent},
+    pnet::{PnetConfig, PreSharedKey},
+    swarm::NetworkBehaviourEventProcess,
+    yamux::Config as YamuxConfig,
+    NetworkBehaviour,
+};
+use libp2p_core::{
+    either::EitherTransport, identity, transport::upgrade, PeerId, StreamMuxer, Transport,
+};
+use libp2p_noise::{Keypair, NoiseConfig, X25519Spec};
+use libp2p_tcp::TcpConfig;
+use std::{error::Error, time::Duration};
+
+#[derive(NetworkBehaviour)]
+pub struct CustomBehaviour {
+    pub kademlia: Kademlia<MemoryStore>,
+    pub mdns: Mdns,
+    #[behaviour(ignore)]
+    pub ui_data: UiData,
+}
+impl NetworkBehaviourEventProcess<MdnsEvent> for CustomBehaviour {
+    // Called when `mdns` produces an event.
+    fn inject_event(&mut self, event: MdnsEvent) {
+        match event {
+            MdnsEvent::Discovered(list) => {
+                for (peer_id, multiaddr) in list {
+                    trace!("found new peer {}", peer_id.to_string());
+                    self.kademlia.add_address(&peer_id, multiaddr);
+                    self.ui_data.send_to_ui(&peer_id);
+                }
+            }
+            MdnsEvent::Expired(expired_addresses) => {
+                for (peer_id, multi_addr) in expired_addresses {
+                    self.kademlia.remove_address(&peer_id, &multi_addr);
+                    self.ui_data.remove(&peer_id);
+                }
+            }
+        }
+    }
+}
+impl NetworkBehaviourEventProcess<KademliaEvent> for CustomBehaviour {
+    // Called when `kademlia` produces an event.
+    fn inject_event(&mut self, message: KademliaEvent) {
+        match message {
+            KademliaEvent::QueryResult { result, .. } => match result {
+                QueryResult::GetRecord(Ok(ok)) => {
+                    for PeerRecord {
+                        record: Record { key, value, .. },
+                        ..
+                    } in ok.records
+                    {
+                        info!(
+                            "Got record {:?} {:?}",
+                            std::str::from_utf8(key.as_ref()).unwrap(),
+                            std::str::from_utf8(&value).unwrap(),
+                        );
+                    }
+                }
+                QueryResult::GetRecord(Err(err)) => {
+                    error!("Failed to get record: {:?}", err);
+                }
+                QueryResult::PutRecord(Ok(PutRecordOk { key })) => {
+                    trace!(
+                        "Successfully put record {:?}",
+                        std::str::from_utf8(key.as_ref()).unwrap()
+                    );
+                }
+                QueryResult::PutRecord(Err(err)) => {
+                    error!("Failed to put record: {:?}", err);
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+}
+
+pub fn build_noise_transport(
+    key_pair: &identity::Keypair,
+    psk: Option<PreSharedKey>,
+) -> impl Transport<
+    Output = (
+        PeerId,
+        impl StreamMuxer<
+                OutboundSubstream = impl Send,
+                Substream = impl Send,
+                Error = impl Into<io::Error>,
+            > + Send
+            + Sync,
+    ),
+    Error = impl Error + Send,
+    Listener = impl Send,
+    Dial = impl Send,
+    ListenerUpgrade = impl Send,
+> + Clone {
+    let dh_keys = Keypair::<X25519Spec>::new()
+        .into_authentic(&key_pair)
+        .unwrap();
+    let noise_config = NoiseConfig::xx(dh_keys).into_authenticated();
+    let yamux_config = YamuxConfig::default();
+
+    let base_transport = TcpConfig::new().nodelay(true);
+    let maybe_encrypted = match psk {
+        Some(psk) => EitherTransport::Left(
+            base_transport.and_then(move |socket, _| PnetConfig::new(psk).handshake(socket)),
+        ),
+        None => EitherTransport::Right(base_transport),
+    };
+    maybe_encrypted
+        .upgrade(upgrade::Version::V1)
+        .authenticate(noise_config)
+        .multiplex(yamux_config)
+        .timeout(Duration::from_secs(20))
+}
