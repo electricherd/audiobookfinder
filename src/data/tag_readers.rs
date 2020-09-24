ad1b115c939a6437e9b77f39ac916f849bba0500
electricherd <electricherd@users.noreply.github.com>
2020-09-24 18:35:36 +0200

    [collection] updated mp4ameta interface, changed internal interface

--- src/data/tag_readers.rs
+++ src/data/tag_readers.rs
@@ -12,12 +12,12 @@ pub struct CommonAudioInfo {
     pub artist: String,
     pub duration: Duration,
     pub album: Option<String>,
-    pub track: Option<u32>,
+    pub track: Option<u16>,
     pub album_artist: Option<String>,
     pub genre: Option<String>,
-    pub disc: Option<u32>,
-    pub total_discs: Option<u32>,
-    pub total_tracks: Option<u32>,
+    pub disc: Option<u16>,
+    pub total_discs: Option<u16>,
+    pub total_tracks: Option<u16>,
     pub year: Option<i32>,
 }
 
@@ -33,14 +33,7 @@ impl<'a> TagReader<'a> for MP4TagReader {
         match mp4tag::read_from(file_buffer.get_mut()) {
             Ok(tag) => {
                 // track info need extra treatment in mp4ameta
-                let try_track_info = tag.track_number();
-                let (track, total_tracks) = match try_track_info {
-                    Some(good_track_info) => {
-                        let (track, total_tracks) = good_track_info;
-                        (Some(track as u32), Some(total_tracks as u32))
-                    }
-                    None => (None, None),
-                };
+                let (track, total_tracks) = tag.track_number();
                 // year needs extra treatment
                 let year = tag
                     .year()
@@ -81,12 +74,12 @@ impl<'a> TagReader<'a> for ID3TagReader {
                     artist: tag.artist().unwrap_or("").to_string(),
                     duration: Duration::from_secs(tag.duration().unwrap_or(0) as u64),
                     album: tag.album().map(|s| s.to_string()),
-                    track: tag.track(),
+                    track: tag.track().and_then(|v| Some(v as u16)),
                     album_artist: tag.album_artist().map(|st| st.to_string()),
                     genre: tag.genre().map(|s| s.to_string()),
-                    disc: tag.disc(),
-                    total_discs: tag.total_discs(),
-                    total_tracks: tag.total_tracks(),
+                    disc: tag.disc().and_then(|v| Some(v as u16)),
+                    total_discs: tag.total_discs().and_then(|v| Some(v as u16)),
+                    total_tracks: tag.total_tracks().and_then(|v| Some(v as u16)),
                     year: tag.year(),
                 };
                 Ok(info)
@@ -127,13 +120,13 @@ impl<'a> TagReader<'a> for FlacTagReader {
                                 // todo: duration somewhere else
                                 duration: Duration::from_secs(0),
                                 album: take_first_or_option(tag.album()),
-                                track: tag.track(),
+                                track: tag.track().and_then(|v| Some(v as u16)),
                                 album_artist: take_first_or_option(tag.album_artist()),
                                 genre: take_first_or_option(tag.genre()),
                                 disc: Some(0), // tag.disc(),
                                 // todo: discs, total discs, year somewhere else??
                                 total_discs: Some(0), // tag.total_discs(),
-                                total_tracks: tag.total_tracks(),
+                                total_tracks: tag.total_tracks().and_then(|v| Some(v as u16)),
                                 year: Some(0), //tag.year(),
                             });
                         }
