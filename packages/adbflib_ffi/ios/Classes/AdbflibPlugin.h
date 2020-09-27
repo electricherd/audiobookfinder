b82d53b51e3e879a2bf9491c98a0fcaaa22ff59c
electricherd <electricherd@users.noreply.github.com>
2020-09-27 23:15:06 +0200

    [flutterust] added base structure for android

---
+++ packages/adbflib_ffi/ios/Classes/AdbflibPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface AdbflibPlugin : NSObject<FlutterPlugin>
+@end
---
+++ packages/adbflib_ffi/lib/adbflib.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'dart:ffi';
+import 'package:ffi/ffi.dart';
+import 'package:isolate/ports.dart';
+
+import 'ffi.dart' as native;
+
+class Adbflib {
+  static setup() {
+    native.store_dart_post_cobject(NativeApi.postCObject);
+    print("Adbflib Setup Done");
+  }
+
+  Future<int> fileCountGood(String path) {
+    var pathPointer = Utf8.toUtf8(path);
+    final completer = Completer<int>();
+    final sendPort = singleCompletePort(completer);
+    final res = native.file_count_good(
+      sendPort.nativePort,
+      pathPointer,
+    );
+    if (res != 1) {
+      _throwError();
+    }
+    return completer.future;
+  }
+
+  void _throwError() {
+    final length = native.last_error_length();
+    final Pointer<Utf8> message = allocate(count: length);
+    native.error_message_utf8(message, length);
+    final error = Utf8.fromUtf8(message);
+    print(error);
+    throw error;
+  }
+}
